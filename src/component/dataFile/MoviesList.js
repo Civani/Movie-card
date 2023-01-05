1942f7c8a97a78db038938d30756c4907c100162
Shivani Tiwari <104130058+Civani@users.noreply.github.com>
2023-01-05 10:15:03 +0530

    Add files via upload

---
+++ src/component/dataFile/MoviesList.js
@@ -0,0 +1,67 @@
+export const List = [
+  {
+    id: 1,
+    title: "Avatar",
+    distributor: "20th Century Fox",
+    year: 2009,
+    amount: "$2,787,965,087",
+    img: {
+      src:
+        "https://1.bp.blogspot.com/-gelV3BnEf2g/XrgKvH31t0I/AAAAAAAADxc/fzVnzw83lXY6zCmmEJD6UTDzuljSAQjfQCK4BGAsYHg/star%2Btrek%2Bhindi%2Benglish%2Bdubbed%2B480p%2B720p%2B300mb%2Bmovie%2Bbtmovie.jpg",
+      alt: "avatar"
+    },
+    ranking: 1
+  },
+  {
+    id: 2,
+    title: "Titanic",
+    distributor: "20th Century Fox",
+    year: 1997,
+    amount: "$2,187,463,944",
+    img: {
+      src:
+        "https://th.bing.com/th/id/OIP.2u5rOKL5nQsXSzd0yBi6LwHaLE?pid=ImgDet&rs=1",
+      alt: "titanic"
+    },
+    ranking: 2
+  },
+  {
+    id: 3,
+    title: "Star Wars: The Force Awakens",
+    distributor: "Walt Disney Studios Motion Pictures",
+    year: 2015,
+    amount: "$2,068,223,624",
+    img: {
+      src:
+        "https://th.bing.com/th/id/OIP.qkOowoiRfmWCa9EEAro9ZgHaF8?pid=ImgDet&rs=1",
+      alt: "star_wars_the_force_awakens"
+    },
+    ranking: 3
+  },
+  {
+    id: 4,
+    title: "Avengers: Infinity War",
+    distributor: "Walt Disney Studios Motion Pictures",
+    year: 2018,
+    amount: "$2,048,359,754",
+    img: {
+      src:
+        "https://th.bing.com/th/id/OIP.1jdklJKgjAe81COtBdjG9AHaLH?pid=ImgDet&rs=1",
+      alt: "avengers_infinity_war"
+    },
+    ranking: 4
+  },
+  {
+    id: 5,
+    title: "Jurassic World",
+    distributor: "Universal Pictures",
+    year: 2015,
+    amount: "$1,671,713,208",
+    img: {
+      src:
+        "https://th.bing.com/th/id/R.18d512f2be044e566db6494a89817371?rik=pJwdB%2fnLBO8mUA&riu=http%3a%2f%2fwww.hdwallpapers.in%2fwalls%2fjurassic_world_2015_movie-wide.jpg&ehk=av8aD0yQ8g40FSkXPlV%2f%2fUXNWxmq93qTcfWYmDE0yhg%3d&risl=&pid=ImgRaw&r=0",
+      alt: "jurassic_world"
+    },
+    ranking: 5
+  }
+];
